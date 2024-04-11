c3e13bd736d8560ec3249692ca91cc10685b3380
Phyrone <phyrone@phyrone.de>
2024-04-11 13:42:09 +0200

    🎨 Fmt

--- src/routes/(main)/(content)/cube/+page.svelte
+++ src/routes/(main)/(content)/cube/+page.svelte
@@ -1,81 +1,68 @@
 <script lang="ts">
-  import TestScene from "./TestScene.svelte";
-  import { Canvas } from "@threlte/core";
+	import TestScene from './TestScene.svelte';
+	import { Canvas } from '@threlte/core';
 
+	let rotation: number = 0;
+	let rotate: boolean = false;
+	let controll: boolean = false;
 
-  let rotation: number = 0;
-  let rotate: boolean = false;
-  let controll: boolean = false;
+	//when space is pressed, toggle rotation
+	function handle_keypress(e: KeyboardEvent) {
+		if (e.code === 'Space' && !e.repeat && !pressed_keys.includes('KeyR')) {
+			rotate = !rotate;
+		} else if (e.code === 'KeyR') {
+			rotation = 0;
+			rotate = false;
+		} else if (e.code === 'KeyC' && !e.repeat) {
+			controll = !controll;
+		}
+	}
 
-  //when space is pressed, toggle rotation
-  function handle_keypress(e: KeyboardEvent) {
-    if (e.code === "Space" && !e.repeat && !pressed_keys.includes("KeyR")) {
-      rotate = !rotate;
-    } else if (e.code === "KeyR") {
-      rotation = 0;
-      rotate = false;
-    } else if (e.code === "KeyC" && !e.repeat) {
-      controll = !controll;
-    }
-  }
+	let pressed_keys: string[] = [];
 
+	function handle_key_down(e: KeyboardEvent) {
+		if (!pressed_keys.includes(e.code)) {
+			pressed_keys = [...pressed_keys, e.code];
+		}
+	}
 
-  let pressed_keys: string[] = [];
-
-  function handle_key_down(e: KeyboardEvent) {
-    if (!pressed_keys.includes(e.code)) {
-      pressed_keys = [...pressed_keys, e.code];
-    }
-  }
-
-  function handle_key_up(e: KeyboardEvent) {
-    pressed_keys = pressed_keys.filter((key) => key !== e.code);
-  }
-
+	function handle_key_up(e: KeyboardEvent) {
+		pressed_keys = pressed_keys.filter((key) => key !== e.code);
+	}
 </script>
 
 <svelte:window
-  on:keypress={handle_keypress}
-  on:keydown={handle_key_down}
-  on:keyup={handle_key_up}
+	on:keypress={handle_keypress}
+	on:keydown={handle_key_down}
+	on:keyup={handle_key_up}
 />
 <svelte:head>
-  <title>3D Cube Test</title>
+	<title>3D Cube Test</title>
 </svelte:head>
 
 <div class="flex-none flex flex-wrap">
-  <div class="flex-auto">
-    Rotate:
-    <kbd
-      class="kbd"
-      class:bg-primary={rotate}
-      class:bg-primary-content={rotate}
-    >Space</kbd>
-  </div>
-  <div class="flex-auto">
-    Enable Orbit Control:
-    <kbd
-      class="kbd"
-      class:bg-primary={controll}
-      class:bg-primary-content={controll}
-    >C</kbd>
-  </div>
-  <div class="flex-auto">
-    Reset Rotation:
-    <kbd
-      class="kbd"
-      class:bg-primary={pressed_keys.includes("KeyR")}
-      class:bg-primary-content={pressed_keys.includes("KeyR")}
-    >R</kbd>
-  </div>
-
+	<div class="flex-auto">
+		Rotate:
+		<kbd class="kbd" class:bg-primary={rotate} class:bg-primary-content={rotate}>Space</kbd>
+	</div>
+	<div class="flex-auto">
+		Enable Orbit Control:
+		<kbd class="kbd" class:bg-primary={controll} class:bg-primary-content={controll}>C</kbd>
+	</div>
+	<div class="flex-auto">
+		Reset Rotation:
+		<kbd
+			class="kbd"
+			class:bg-primary={pressed_keys.includes('KeyR')}
+			class:bg-primary-content={pressed_keys.includes('KeyR')}>R</kbd
+		>
+	</div>
 </div>
 
 <div class="flex-auto">
-  {#key controll}
-    <Canvas autoRender={true}>
-      <TestScene bind:rotate bind:controll bind:rotation />
-    </Canvas>
-  {/key}
+	{#key controll}
+		<Canvas autoRender={true}>
+			<TestScene bind:rotate bind:controll bind:rotation />
+		</Canvas>
+	{/key}
 </div>
-
--- src/routes/(main)/(content)/cube/TestScene.svelte
+++ src/routes/(main)/(content)/cube/TestScene.svelte
@@ -1,44 +1,42 @@
 <script lang="ts">
-  import { T, useTask } from "@threlte/core";
+	import { T, useTask } from '@threlte/core';
 
-  let OrbitControlsPromise = import("@threlte/extras").then((mod) => mod.OrbitControls);
+	let OrbitControlsPromise = import('@threlte/extras').then((mod) => mod.OrbitControls);
 
-  export let rotation = 0;
-  export let rotate = false;
-  export let controll = false;
+	export let rotation = 0;
+	export let rotate = false;
+	export let controll = false;
 
-  // useTask is relying on a context provided
-  // by <Canvas>. Because we are definitely *inside*
-  // <Canvas>, we can safely use it.
-  useTask((delta) => {
-    if (rotate) {
-      rotation += delta;
-    }
-  });
-
+	// useTask is relying on a context provided
+	// by <Canvas>. Because we are definitely *inside*
+	// <Canvas>, we can safely use it.
+	useTask((delta) => {
+		if (rotate) {
+			rotation += delta;
+		}
+	});
 </script>
 
-
 <T.PerspectiveCamera
-  makeDefault
-  position={[2, 2, 2]}
-  on:create={({ ref }) => {
-    ref.lookAt(0, 0, 0)
-  }}
+	makeDefault
+	position={[2, 2, 2]}
+	on:create={({ ref }) => {
+		ref.lookAt(0, 0, 0);
+	}}
 >
-  {#if controll}
-    {#await OrbitControlsPromise}
-      <!-- do nothing -->
-    {:then OrbitControls}
-      <OrbitControls />
-    {/await}
-  {/if}
+	{#if controll}
+		{#await OrbitControlsPromise}
+			<!-- do nothing -->
+		{:then OrbitControls}
+			<OrbitControls />
+		{/await}
+	{/if}
 </T.PerspectiveCamera>
 
-<T.AmbientLight color={0xFFFFFF} intensity={1.2} />
+<T.AmbientLight color={0xffffff} intensity={1.2} />
 <T.DirectionalLight position={[0, 10, 10]} castShadow />
 
 <T.Mesh rotation.x={rotation} rotation.y={rotation} rotation.z={rotation}>
-  <T.BoxGeometry />
-  <T.MeshStandardMaterial color={0x0E7277} />
+	<T.BoxGeometry />
+	<T.MeshStandardMaterial color={0x0e7277} />
 </T.Mesh>
